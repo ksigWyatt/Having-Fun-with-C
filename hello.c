b795517595fd08f57520483115432e54371d3861
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-06-14 11:00:21 -0400

    Initial Commit
    Adding Readme / iterables

---
+++ hello.c
@@ -0,0 +1,16 @@
+/*
+	New text C file
+*/
+#include <studio.h>
+
+int main() {
+
+	//incrementing through i
+	for (int i = 0; i< 10; i++) {
+		printf(i);
+	}
+
+	//printing Hello, World!
+   	printf("Hello, World!");
+   	return 0;
+}
